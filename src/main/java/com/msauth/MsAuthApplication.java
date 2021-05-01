8bc23dd3221315ccb4f7f4dc1fb1b75e6fe524fc
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2021-05-01 15:38:05 -0300

    Update cors

--- src/main/java/com/msauth/MsAuthApplication.java
+++ src/main/java/com/msauth/MsAuthApplication.java
@@ -18,7 +18,7 @@ public class MsAuthApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
 	}
